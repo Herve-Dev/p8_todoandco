96399dbe266320ec9b8ee052b7aecfb8db5485c8
FullMetal-Dev <madinina972represent@gmail.com>
2023-07-19 17:30:40 +0200

    add partial flash for alert message

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,7 +36,8 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
+            
+            $this->addFlash('success', "L'utilisateur a bien été ajouté.");
 
             return $userAuthenticator->authenticateUser(
                 $user,
