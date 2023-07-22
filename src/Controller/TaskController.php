8c60b16f34d8af86f0d980989cc06d1b36465c53
FullMetal-Dev <madinina972represent@gmail.com>
2023-07-22 23:22:18 +0200

    create taskType form with twig and controller

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
 use App\Repository\TaskRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
@@ -18,5 +22,28 @@ class TaskController extends AbstractController
             'controller_name' => 'TaskController',
             'tasks' => $tasks
         ]);
+    }
+
+    #[Route('/tasks/create', name: 'task_create')]
+    public function createAction(
+        Request $request,
+        EntityManagerInterface $em)
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
     }
 }
