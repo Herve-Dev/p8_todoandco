2c42b3cf2dc21d2da8521bcb995ce82cc37c41db
FullMetal-Dev <madinina972represent@gmail.com>
2023-07-24 01:34:46 +0200

    new file with crud for task

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use function Symfony\Component\Clock\now;
+
 class TaskController extends AbstractController
 {
     #[Route('/tasks', name: 'task_list')]
@@ -36,6 +38,10 @@ class TaskController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
+            $task->setCreatedAt(now());
+            $task->setIsDone(false);
+            $task->setUser($this->getUser());
+
             $em->persist($task);
             $em->flush();
 
@@ -45,5 +51,59 @@ class TaskController extends AbstractController
         }
 
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route("/tasks/{id}/edit", name: 'task_edit')]
+    public function editAction(
+        Task $task, 
+        Request $request,
+        EntityManagerInterface $em)
+    {
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash('success', 'La tâche a bien été modifiée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form->createView(),
+            'task' => $task,
+        ]);
+    }
+
+    #[Route("/tasks/{id}/toggle", name: 'task_toggle')]
+    public function toggleTaskAction(Task $task, EntityManagerInterface $em)
+    {
+        $isDoneBeforeToggle = $task->isDone();
+        $task->toggle(!$isDoneBeforeToggle);
+        
+        $em->flush();
+
+        if ($isDoneBeforeToggle && !$task->isDone()) {
+            $this->addFlash('warning', sprintf('La tâche %s est maintenant marquée non faite.', $task->getTitle()));
+        } elseif (!$isDoneBeforeToggle && $task->isDone()) {
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+        }
+
+        return $this->redirectToRoute('task_list');
+    }
+
+    #[Route("/tasks/{id}/delete", name: 'task_delete')]
+    public function deleteTaskAction(Task $task, EntityManagerInterface $em)
+    {
+        $em->remove($task);
+        $em->flush();
+
+        $this->addFlash('success', 'La tâche a bien été supprimée.');
+
+        return $this->redirectToRoute('task_list');
     }
 }
