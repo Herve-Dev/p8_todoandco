0f9777aaad2ce9d6a87d26e505fbd2bfcbc84685
FullMetal-Dev <madinina972represent@gmail.com>
2023-07-26 22:03:56 +0200

    create route /users with logic controller and template twig list

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/users', name: 'user_list')]
+    public function listAction(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $users = $userRepository->findAll();
+
+        return $this->render('user/list.html.twig', [
+            'users' => $users,
         ]);
     }
 }
