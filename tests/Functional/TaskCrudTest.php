ddfbef1d2de27945d1c01688331d97a6148b2edd
FullMetal-Dev <madinina972represent@gmail.com>
2023-08-20 01:39:57 +0200

    add Makefile for test and add test for admon modify role user

--- tests/Functional/TaskCrudTest.php
+++ tests/Functional/TaskCrudTest.php
@@ -16,7 +16,7 @@ class TaskCrudTest extends WebTestCase
         $urlGenerator = $client->getContainer()->get("router");
         $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
 
-        $user = $entityManager->find(User::class, 64);
+        $user = $entityManager->find(User::class, 1);
         $client->loginUser($user);
 
         $client->request(Request::METHOD_GET, $urlGenerator->generate('task_list'));
@@ -30,7 +30,7 @@ class TaskCrudTest extends WebTestCase
         $urlGenerator = $client->getContainer()->get("router");
         $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
 
-        $user = $entityManager->find(User::class, 64);
+        $user = $entityManager->find(User::class, 1);
         $client->loginUser($user);
 
         $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('task_create'));
@@ -53,7 +53,7 @@ class TaskCrudTest extends WebTestCase
         $urlGenerator = $client->getContainer()->get("router");
         $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
 
-        $user = $entityManager->find(User::class, 64);
+        $user = $entityManager->find(User::class, 1);
         $task = $entityManager->getRepository(Task::class)->findOneBy([
             'user' => $user
         ]);
@@ -86,7 +86,7 @@ class TaskCrudTest extends WebTestCase
         $urlGenerator = $client->getContainer()->get("router");
         $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
 
-        $user = $entityManager->find(User::class, 64);
+        $user = $entityManager->find(User::class, 1);
         $task = $entityManager->getRepository(Task::class)->findOneBy([
             'user' => $user
         ]);
